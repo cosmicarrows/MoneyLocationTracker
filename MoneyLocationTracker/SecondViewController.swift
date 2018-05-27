b3b8f2953ff57cb1d34da77e829132f59c7005be
Laurence Wingo <laurence@cosmicarrows.com>
2018-05-27 09:53:11 -0400

    Initial Commit

---
+++ MoneyLocationTracker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MoneyLocationTracker
+//
+//  Created by Laurence Wingo on 5/27/18.
+//  Copyright © 2018 Cosmic Arrows, LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
